ccef9ae02e407d2bbab421ee9db2f9c5150e6046
Carl Peters <carlpeters@MacBook-Pro-van-Carl.local>
2022-02-06 16:15:43 +0100

    new project with lots of pattersn

---
+++ src/test/java/edu/pattern/abstractfactory/TestMultilingual.java
@@ -0,0 +1,44 @@
+package edu.pattern.abstractfactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import edu.pattern.abstractfactory.Questions;
+import edu.pattern.abstractfactory.Greetings;
+import org.junit.jupiter.api.Test;
+
+public class TestMultilingual {
+
+    @Test
+    public void test_en() {
+
+        // single decision point
+        MessagesAbstractFactory abstractFactory = new MessagesEnFactory();
+
+        Questions questions = abstractFactory.getQuestions();
+
+        assertEquals("what time is it?", questions.askTime() );
+        assertEquals("how is the weather?", questions.askWeather() );
+
+        Greetings greetings = abstractFactory.getGreetings();
+
+        assertEquals("good morning", greetings.goodMorning());
+        assertEquals("good afternoon", greetings.goodAfternoon());
+    }
+
+    @Test
+    public void test_es() {
+
+        // single decision point
+        MessagesAbstractFactory abstractFactory = new MessagesEsFactory();
+
+        Questions questions = abstractFactory.getQuestions();
+
+        assertEquals("¿qué hora es?", questions.askTime() );
+        assertEquals("¿qué tiempo hace?", questions.askWeather() );
+
+        Greetings greetings = abstractFactory.getGreetings();
+
+        assertEquals("buenos días", greetings.goodMorning());
+        assertEquals("buenas tardes", greetings.goodAfternoon());
+    }
+
+}
